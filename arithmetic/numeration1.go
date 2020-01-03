6893d20eab2694259528f0a9dc0829977a58bdb7
liugang008 <liugang008@ke.com>
2020-01-03 23:43:32 +0800

    complete

---
+++ arithmetic/numeration1.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"strings"
+)
+
+func trans2Two(num int) string{
+	ret := make([]string, 0)
+	for num > 0 {
+		if num % 2 == 1{
+			ret = append(ret, "1")
+		} else {
+			ret = append(ret, "0")
+		}
+		num = num / 2
+	}
+
+	tmp := make([]string, 0)
+	for i := len(ret) - 1; i >= 0; i-- {
+		tmp = append(tmp, ret[i])
+	}
+
+	return strings.Join(tmp, "")
+}
