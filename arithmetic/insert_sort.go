72a7092da6ef4075994a1d053525dfd95a8a332f
i6448038 <376832293@qq.com>
2024-06-08 20:31:07 +0800

    插入排序修正

--- arithmetic/insert_sort.go
+++ arithmetic/insert_sort.go
@@ -3,23 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	array := []int{5, 3, 4, 6, 2}
+	array := []int{5, 3, 4, 1, 6, 2}
 	fmt.Println(InsertionSort(array))
 }
 
 //插入排序
 //5               3            将 3 插入只有一个元素 5 的有序表中
 //3 5             4            将 4 插入有两个元素 3 5 的有序表中
-//3 4 5           6            将 6 插入有两个元素 3 4 5 的有序表中
-//3 4 5 6         2            将 2 插入有两个元素 3 4 5 6 的有序表中
-//2 3 4 5 6
+//3 4 5           1            将 1 插入有两个元素 3 4 5 的有序表中
+//1 3 4 5         6            将 6 插入有两个元素 1 3 4 5 的有序表中
+//1 3 4 5 6       2            将 2 插入有两个元素 1 3 4 5 6 的有序表中
+//1 2 3 4 5 6
 func InsertionSort(messArray []int) []int {
 	for i := 1; i < len(messArray); i++ {
-		for j := i; j > 0; j-- {
-			if messArray[j] < messArray[j-1] {
-				messArray[j], messArray[j-1] = messArray[j-1], messArray[j]
-			}
+		key := messArray[i]
+		j := i - 1
+		for j >= 0 && messArray[j] > key {
+			messArray[j+1] = messArray[j]
+			j--
 		}
+		messArray[j+1] = key
+
 	}
 	return messArray
 }
